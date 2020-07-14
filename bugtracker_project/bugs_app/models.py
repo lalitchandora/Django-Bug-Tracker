aa800c0f7367f3c1a5fbab0acc5cb77adcac45f3
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-14 22:46:29 +0530

    Model 'bug' added

--- bugtracker_project/bugs_app/models.py
+++ bugtracker_project/bugs_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from projects_app.models import Project
 
-# Create your models here.
+class Bug(models.Model):
+
+    PRIORITY_CHOICES = [
+        ('LO','Low'),
+        ('MD','Medium'),
+        ('HI','High')
+    ]
+    STATE_CHOICES = [
+        ('RE','Resolved'),
+        ('UN', 'Unresolved')
+    ]
+
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE, default='0')
+    name = models.CharField(max_length=50)
+    desc = models.TextField()
+    priority = models.CharField(max_length=2, choices = PRIORITY_CHOICES)
+    state = models.CharField(max_length=2, choices = STATE_CHOICES)
+
+    def __str__(self):
+        return self.name
