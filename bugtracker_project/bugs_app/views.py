40a6931d86f23e663c3fa62f5492243d555680ed
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-14 22:53:27 +0530

    list_bugs view added

--- bugtracker_project/bugs_app/views.py
+++ bugtracker_project/bugs_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Bug
+from projects_app.models import Project
 
-# Create your views here.
+def list_bugs(request,id):
+    query_bugs = Bug.objects.filter(project_id=id)
+    project_name = query_bugs[0].project_id.name
+    context = {
+        'bugs_list': query_bugs,
+        'project_name': project_name
+    }
+    return render(request, 'bugs_app/bugs.html', context)
