4e923eb60940e6b22e02d5b7fe35cd51f025bd11
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-14 13:52:09 +0530

    Project model added

--- bugtracker_project/projects_app/models.py
+++ bugtracker_project/projects_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    organization_name = models.CharField(max_length=70)
+    technology = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
