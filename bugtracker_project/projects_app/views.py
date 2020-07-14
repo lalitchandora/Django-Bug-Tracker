98a2191ce52b99ed0e01c8d711aaf3c31dbea403
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-14 13:56:54 +0530

    added addproject view

--- bugtracker_project/projects_app/views.py
+++ bugtracker_project/projects_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProjectForm
 
-# Create your views here.
+def home(request):
+    return render(request,'projects_app/base.html')
+
+def add_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProjectForm()    
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'projects_app/add_project.html',context)
