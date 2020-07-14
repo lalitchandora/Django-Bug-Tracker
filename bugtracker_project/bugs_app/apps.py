11a02004a616f8636ef06de87b8293794715515b
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-14 16:25:12 +0530

    added app "bugs"

---
+++ bugtracker_project/bugs_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BugsAppConfig(AppConfig):
+    name = 'bugs_app'
--- bugtracker_project/projects_app/urls.py
+++ bugtracker_project/projects_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from projects_app import views
+
 urlpatterns = [
     path('',views.home,name='home'),
     path('createproject/',views.add_project,name='addproject')
