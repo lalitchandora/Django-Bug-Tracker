734084d561732ac52d82bb2ecba7592f3dd60899
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-14 16:45:08 +0530

    added bugs_app to Installed apps

--- bugtracker_project/bugtracker_project/settings.py
+++ bugtracker_project/bugtracker_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects_app.apps.ProjectsAppConfig',
+    'bugs_app.apps.BugsAppConfig',
 ]
 
 MIDDLEWARE = [
