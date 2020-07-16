e276c9c8287a8c4836e8868ab6a4fc5943b0c61f
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-16 20:00:01 +0530

    adding delete path for bugs

--- bugtracker_project/bugs_app/urls.py
+++ bugtracker_project/bugs_app/urls.py
@@ -3,5 +3,6 @@ from bugs_app import views
 
 urlpatterns = [
     path('<int:id>/',views.list_bugs,name='bug_list'),
-    path('addbug/<int:project_id>/',views.bug_add,name='bugform')
+    path('addbug/<int:project_id>/',views.bug_add,name='bugform'),
+    path('delete/<int:project_id>/<int:bug_id>',views.bug_delete,name = 'bug_delete')
 ]
