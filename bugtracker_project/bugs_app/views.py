97ef51ffffc91de52d7b2dee45fb98c95b48c2b7
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-16 14:55:01 +0530

    added bug_add view for displaying form for adding bugs

--- bugtracker_project/bugs_app/views.py
+++ bugtracker_project/bugs_app/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Bug
 from projects_app.models import Project
+from .forms import AddBugForm
 
 def list_bugs(request,id):
     query_bugs = Bug.objects.filter(project_id=id)
-    project_name = query_bugs[0].project_id.name
+    project = Project.objects.filter(id = id)[0]
+
     context = {
         'bugs_list': query_bugs,
-        'project_name': project_name
+        'project': project
     }
     return render(request, 'bugs_app/bugs.html', context)
+
+def bug_add(request,project_id):
+
+    if request.method == "POST":
+        form = AddBugForm(request.POST)
+        if form.is_valid():
+            project = Project.objects.filter(id=project_id)[0]
+
+            bug = form.save(commit = False)
+            bug.project_id = project
+            bug.save()
+            a = reverse('bug_list', args=[project_id])
+            
+            return redirect(a)
+            
+    else:
+        form = AddBugForm()
+        
+    context = {
+        'form': form
+    }
+
+    return render(request,'bugs_app/addbugs.html', context)
