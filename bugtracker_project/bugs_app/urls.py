e5ef519f30329d019ec4302c1d1866faebe07bd7
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-16 14:53:41 +0530

    added path for bugForm

--- bugtracker_project/bugs_app/urls.py
+++ bugtracker_project/bugs_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from bugs_app import views
 
 urlpatterns = [
-    path('<int:id>/',views.list_bugs),
+    path('<int:id>/',views.list_bugs,name='bug_list'),
+    path('addbug/<int:project_id>/',views.bug_add,name='bugform')
 ]
