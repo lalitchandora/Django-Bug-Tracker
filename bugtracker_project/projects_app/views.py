4a794c632ff7520d007631cf1e1edf55f88b9f77
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-21 10:25:46 +0530

    Added pagination for project list

--- bugtracker_project/projects_app/views.py
+++ bugtracker_project/projects_app/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import ProjectForm
 from .models import Project
 
 def home(request):
     project_list = Project.objects.all()
+    page = request.GET.get('page',1)
+
+    paginator = Paginator(project_list,2)
+    try:
+        project_list = paginator.page(page)
+    except PageNotAnInteger:
+        project_list = paginator.page(1)
+    except EmptyPage:
+        project_list = paginator.page(paginator.num_pages)
     context = {
         'projects': project_list,
     }
